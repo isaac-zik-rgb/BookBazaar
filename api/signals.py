274f28e9b5b59997b1d35f4a1aada11d0e93b490
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-02-21 18:13:13 +0100

    Add user profile model and serializer

---
+++ api/signals.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save
+from .models import UserProfile
+from django.dispatch import receiver
+
+# a function that create the user profile when the user is creatd with some data
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, email=instance.email, first_name=instance.first_name, last_name=instance.last_name, username=instance.username, password=instance.password)
--- api/views.py
+++ api/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserSerializer,RegisterSerializer
+from .serializers import UserSerializer,RegisterSerializer, UserProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import generics
+from rest_framework import generics, status
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
@@ -18,15 +18,32 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 @permission_classes([IsAuthenticated])
 @swagger_auto_schema(
     responses={200: UserSerializer},
-    operation_description="Get details of the currently logged-in user"
+    operation_description=" Retrieve the profile of the currently logged-in user."
 )
-def user_detail_api(request):
+def profile_details(request):
     """
-    Get details of the currently logged-in user.
+     Retrieve the profile of the currently logged-in user.
     """
-    user = User.objects.get(id=request.user.id)
-    serializer = UserSerializer(user)
+    user = request.user
+    serializer = UserProfileSerializer(user.profile, many=False)
     return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+@swagger_auto_schema(
+    responses={200: UserSerializer},
+    operation_description=" Update the profile of the currently logged-in user."
+)
+def update_profile(request):
+    """
+     Update the profile of the currently logged-in user.
+    """
+    user = request.user
+    serializer = UserProfileSerializer(user.profile, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Class based view to register user
 class RegisterUserAPIView(generics.CreateAPIView):
