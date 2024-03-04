b794a19903a1ebc4e679cd693a215a88e70c37d9
Didimukhtar <jamilatmukhtar2014@gmail.com>
2024-03-04 13:48:27 +0100

    add signup and login pages

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import Home from 'pages/Home';
 
 function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: <Home />
     },
+    {
+      path: '/signup',
+      element: <Signup />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    }
   ]);
 
   return <RouterProvider router={router} />;
