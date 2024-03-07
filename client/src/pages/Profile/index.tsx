34cca9c605030ed12a75b919e33d13245394c345
Didimukhtar <jamilatmukhtar2014@gmail.com>
2024-03-07 21:08:02 +0100

    add function to disable input field on save

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './style.css'
 import profile_icon from 'assets/profileimage.png'
 
 const Profile = () => {
+    const [editing, setEditing] = useState(false);
+
   return (
     <div className='container'>
+        <button className='edit-button' onClick={()=> {setEditing(!editing)}}>
+            {editing ? 'Save' : 'Edit'}
+            </button>
         <div className='header_image'>
             <img src={profile_icon} alt=''/>
+            <input type="file" name='file' id='file' />
+            <label htmlFor="file">CHOOSE PIC</label>
         </div>
         <div className='form'>
             <form>
             <label htmlFor="firstname">First Name</label> 
                     <input 
                         type="text"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="firstname"
                         id="firstname"
                         placeholder="Enter First Name"
@@ -21,6 +31,8 @@ const Profile = () => {
                     <label htmlFor="lastname">Last Name</label> 
                     <input 
                         type="text"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="lastname"
                         id="lastname"
                         placeholder="Enter Last Name"
@@ -30,6 +42,8 @@ const Profile = () => {
                     <label htmlFor="username">Username</label> 
                     <input 
                         type="text"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="username"
                         id="username"
                         placeholder="Enter Username"
@@ -39,6 +53,8 @@ const Profile = () => {
                     <label htmlFor="email">Enter Email </label> 
                     <input 
                         type="email"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="email"
                         id="email"
                         placeholder="Enter Email"
@@ -48,6 +64,8 @@ const Profile = () => {
                     <label htmlFor="phone_number">Contact</label> 
                     <input 
                         type="phone_number"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="phone_number"
                         id="phone_number"
                         placeholder="Enter Mobile number"
@@ -57,6 +75,8 @@ const Profile = () => {
                     <label htmlFor="address">Enter Address </label> 
                     <input 
                         type="address"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="address"
                         id="address"
                         placeholder="Enter Address"
@@ -66,14 +86,17 @@ const Profile = () => {
                     <label htmlFor="favorite_genres">Enter Favorite genres </label> 
                     <input 
                         type="favorite_genres"
+                        className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}
                         name="favorite_genres"
                         id="favorite_genres"
                         placeholder="Enter favorite genres"
                         required
                     />
                     <br /><br /> 
-                    <label htmlFor="bio">Enter Bio </label> 
-                    <textarea></textarea>
+                    <label htmlFor="bio" >Enter Bio </label> 
+                    <textarea className={!editing ? 'cursor-not-allowed' : '' }
+                        disabled={!editing}></textarea>
                     <br /><br /> 
                     <div className='submit'> 
                     <button>Submit</button>
